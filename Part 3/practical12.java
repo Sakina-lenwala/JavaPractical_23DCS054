1b6e4237694f8d6d47e4c55401f992ef39f681dd
Sakina-lenwala <23dcs054@charusat.edu.in>
2024-08-05 14:48:06 +0530

    Create practical12.java

---
+++ Part 3/practical12.java
@@ -0,0 +1,12 @@
+public class practical12 {
+    public static void main(String[] args) {
+
+        System.out.println("Enter pounds :");
+        int a = Integer.parseInt(args[0]);  
+        int b = Integer.parseInt(args[1]);  
+        int rupee = a * 100; 
+        System.out.println(rupee);  
+    }
+}
+
+
